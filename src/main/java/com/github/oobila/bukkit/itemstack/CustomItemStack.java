f6454de11f810c09edcbef3c2cae3a6569d232ab
BuildTools <unconfigured@null.spigotmc.org>
2024-05-29 22:00:45 +0100

    making getLore return a mutable list

--- src/main/java/com/github/oobila/bukkit/itemstack/CustomItemStack.java
+++ src/main/java/com/github/oobila/bukkit/itemstack/CustomItemStack.java
@@ -3,6 +3,9 @@ package com.github.oobila.bukkit.itemstack;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomItemStack extends ItemStack implements IItemStackProxy<CustomItemStack> {
 
     public CustomItemStack(Material material){
@@ -14,6 +17,11 @@ public class CustomItemStack extends ItemStack implements IItemStackProxy<Custom
         setItemMeta(itemStack.getItemMeta());
         setAmount(itemStack.getAmount());
         setData(itemStack.getData());
+    }
+
+    @Override
+    public List<String> getLore() {
+        return new ArrayList<>(IItemStackProxy.super.getLore());
     }
 
     @Override
--- src/main/java/com/github/oobila/bukkit/itemstack/NonPlayerSkull.java
+++ src/main/java/com/github/oobila/bukkit/itemstack/NonPlayerSkull.java
@@ -9,7 +9,9 @@ import org.bukkit.profile.PlayerTextures;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -75,6 +77,11 @@ public class NonPlayerSkull extends ItemStack implements IItemStackProxy<NonPlay
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public List<String> getLore() {
+        return new ArrayList<>(IItemStackProxy.super.getLore());
     }
 
     public static ItemStackBuilder builder(String textureId) {
