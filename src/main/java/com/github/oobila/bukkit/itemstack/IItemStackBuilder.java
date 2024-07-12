42a1bfcdc9bd16a20a92c70807fd20b88babfe5c
BuildTools <unconfigured@null.spigotmc.org>
2024-07-12 13:32:43 +0100

    fixing enchanted option

--- src/main/java/com/github/oobila/bukkit/itemstack/IItemStackBuilder.java
+++ src/main/java/com/github/oobila/bukkit/itemstack/IItemStackBuilder.java
@@ -7,6 +7,7 @@ import com.github.oobila.bukkit.effects.Effect;
 import com.github.oobila.bukkit.itemstack.effects.ItemBehaviour;
 import com.github.oobila.bukkit.itemstack.effects.ItemSlot;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -165,6 +166,7 @@ public interface IItemStackBuilder<T extends ExtendedItemStack<T>> extends Exten
     @Override
     default T setEnchanted() {
         getItemStack().addUnsafeEnchantment(Enchantment.LUCK, 1);
+        apply(meta -> meta.addItemFlags(ItemFlag.HIDE_ENCHANTS));
         return getReturnObject();
     }
 
